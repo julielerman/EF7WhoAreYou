601911b38b3957dadfa01e82ed300fb9aecaf6e6
julie <julielerman@gmail.com>
2015-10-24 15:30:15 -0400

    removed extraneous folders from unused projects

--- Familiar and Enhancements Console Full NET After/TestProject/UnitTest1.cs
+++ Familiar and Enhancements Console Full NET After/TestProject/UnitTest1.cs
@@ -161,7 +161,6 @@ namespace TestProject
         Assert.AreEqual(0, context.Quotes.Count());
 
       }
-
     }
     [TestMethod]
     public void New_DisconnectedPatterns_AttachNewGraphViaChangeTracker() {
