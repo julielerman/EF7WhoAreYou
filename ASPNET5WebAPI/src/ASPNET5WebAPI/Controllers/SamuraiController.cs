42f2842f7cfcb05d29be00ec961748d39506dd32
julie <julielerman@gmail.com>
2015-10-24 22:20:22 -0400

    new projects after beta8 web tools installed provide correct packagejson and webconfig files. got simple web api to return json graph

---
+++ ASPNET5WebAPI/src/ASPNET5WebAPI/Controllers/SamuraiController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using ASPNET5WebAPI.Model;
+using Microsoft.Data.Entity;
+using Microsoft.Framework.Logging;
+using EF7Samurai.Domain;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASPNET5WebAPI.Controllers
+{
+
+  [Route("api/[controller]")]
+  public class SamuraiController : Controller
+  {
+    [FromServices]
+    public SamuraiContext SamuraiContext { get; set; }
+
+    [FromServices]
+    public ILogger<SamuraiController> Logger { get; set; }
+
+    [HttpGet]
+    public IEnumerable<Samurai> GetAll() {
+      return SamuraiContext.Samurais.Include(s => s.Quotes);
+    }
+
+    [HttpGet("{id:int}", Name = "GetByIdRoute")]
+    public IActionResult GetById(int id) {
+      var samurai = SamuraiContext.Samurais.FirstOrDefault(s => s.Id == id);
+      if (samurai == null) {
+        return HttpNotFound();
+      }
+
+      return new ObjectResult(samurai);
+    }
+  }
+}
---
+++ ASPNET5WebAPI/src/WebApplication1/Controllers/SamuraiController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using ASPNET5WebAPI.Model;
+using Microsoft.Data.Entity;
+using Microsoft.Framework.Logging;
+using EF7Samurai.Domain;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASPNET5WebAPI.Controllers
+{
+
+  [Route("api/[controller]")]
+  public class SamuraiController : Controller
+  {
+    [FromServices]
+    public SamuraiContext SamuraiContext { get; set; }
+
+    [FromServices]
+    public ILogger<SamuraiController> Logger { get; set; }
+
+    [HttpGet]
+    public IActionResult Get() {
+      var samurais= SamuraiContext.Samurais.Include(s => s.Quotes).ToList();
+       return new ObjectResult(samurais);
+     
+     
+    }
+
+    [HttpGet("{id:int}", Name = "GetByIdRoute")]
+    public IActionResult GetById(int id) {
+      var samurai = SamuraiContext.Samurais.FirstOrDefault(s => s.Id == id);
+      if (samurai == null) {
+        return HttpNotFound();
+      }
+
+      return new ObjectResult(samurai);
+    }
+  }
+}
